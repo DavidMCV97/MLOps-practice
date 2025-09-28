a25f0a11f27a3c80533fa2e17c405e8f47692431
David Martin <montanerrusaller@gmail.com>
2025-09-28 17:22:40 -0600

    first commit into the MLOps Project. Project is still unfinished.

---
+++ steps/ingest_data.py
@@ -0,0 +1,40 @@
+import logging
+import pandas as pd
+from zenml import step
+
+class IngestData:
+    '''
+    Ingesting the data from datapath
+    '''
+    def __init__(self, data_path:str):
+        '''
+        Args:
+            data_path: path to data
+        '''
+        self.data_path = data_path
+        
+    def get_data(self):
+        '''
+        Ingesting the data from datapath
+        '''
+        logging.info(f'Ingesting data from {self.data_path}')
+        return pd.read_csv(self.data_path)
+
+@step
+def ingest_df(data_path:str) -> pd.DataFrame:
+    '''
+    Ingesting the data from datapath
+
+    Args:
+        data_path: path to data
+
+    Returns:
+        pd.DataFrame: ingested data
+    '''
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(f'Error while Ingesting Data: {e}')
+        raise e
