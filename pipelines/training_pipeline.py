5b6072cd0d62f5c436010d2e08f07ea02a99b133
David Martin <montanerrusaller@gmail.com>
2025-10-07 15:46:09 -0600

    project finished, so we can commit the results. project ready for local deployment

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -12,4 +12,4 @@ def train_pipeline(data_path: str):
     X_train, X_test, y_train, y_test = clean_df(df)
     model_name = ModelNameConfig()
     model = train_model(X_train, X_test, y_train, y_test, model_name)
-    evaluate_model(model, X_test, y_test)
+    mse, r2, rmse = evaluate_model(model, X_test, y_test)
